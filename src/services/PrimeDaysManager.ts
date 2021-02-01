7b45e7e050f2fd1e1810962fa532008e36876a71
Mirdukkk <vdvmirduk@gmail.com>
2021-02-02 01:05:21 +0300

    feat(prime): add prime days manager

---
+++ src/services/PrimeDaysManager.ts
@@ -0,0 +1,41 @@
+import BaseService from '@src/abstractions/BaseService'
+import Constants from '@src/utils/Constants'
+
+const primesTable = Constants.primesTable
+
+export default class PrimeDaysManager extends BaseService {
+  constructor() {
+    super()
+  }
+
+  async decrease(id: string): Promise<boolean> {
+
+    const prime = await this.client.db.get(primesTable, id)
+
+    if (!prime) return false
+
+    if (prime.nextDecreaseTimestamp < Date.now()) {
+      if (prime.days < 1) return false
+      else {
+        await this.client.db.update(primesTable, id, {
+          days: prime.days - 1,
+          nextDecreaseTimestamp: Date.now() + 24 * 60 * 60 * 1000
+        })
+
+        return true
+      }
+    } else return true
+
+  }
+
+  async set(id: string, days: number) {
+    return this.client.db.update(primesTable, id, { days: days }, { upsert: true })
+  }
+
+  async increase(id: string, days: number) {
+    const prime = await this.client.db.get(primesTable, id).catch(() => { return {} })
+
+    return this.set(id, days + (prime?.days ?? 0))
+  }
+
+}
