1e828b38a13c605301d2e9ce98d18a21c8dc5dad
Mirdukkk <vdvmirduk@gmail.com>
2021-03-05 23:30:08 +0300

    fix(token.controller): fix refresh token endpoint

---
+++ api/dto/refresh-token.dto.ts
@@ -0,0 +1,20 @@
+import TokenDto from '@api/dto/token.dto'
+import { IsOptional, IsString } from 'class-validator'
+import { IsSnowflake } from '@api/decorators/is-snowflake.decorator'
+
+export default class RefreshTokenDto extends TokenDto {
+
+  // @ts-ignore
+  @IsSnowflake({
+    message: 'user_id must be a string (snowflake)'
+  })
+  public user_id!: string
+
+  @IsString()
+  refresh_token!: string
+
+  @IsOptional()
+  // @ts-ignore
+  public code: unknown
+
+}
