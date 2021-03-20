e12b92f46c3a2b8c135ce2253c81a4c15376c818
Mirdukkk <vdvmirduk@gmail.com>
2021-03-20 21:20:31 +0300

    feat(api): delay until then first shard will be created added

--- api/guards/is-shards-loaded.guard.ts
+++ api/guards/is-shards-loaded.guard.ts
@@ -1,17 +1,22 @@
 import {
-  Injectable,
-  InternalServerErrorException,
+  Injectable, InternalServerErrorException,
 } from '@nestjs/common'
 import AuthGuard from '@api/guards/auth.guard'
+import { promisify } from 'util'
+
+const wait = promisify(setTimeout)
 
 @Injectable()
 export default class IsShardsLoadedGuard extends AuthGuard {
   async canActivate(): Promise<boolean> {
 
-    if (!this.manager.shards.size)
-      throw new InternalServerErrorException({
-        message: 'shards haven\'t created yet'
-      })
+    if (!this.manager.shards.size) {
+      await wait(10000)
+      if (!this.manager.shards.size)
+        throw new InternalServerErrorException({
+          message: 'shards haven\'t created yet'
+        })
+    }
 
     return true
   }
