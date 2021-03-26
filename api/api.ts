66c0a0acd152f0331798291bb6f5d8dd8250d682
Mirdukkk <vdvmirduk@gmail.com>
2021-03-26 17:21:12 +0300

    chore(deps): remove unused packages

--- api/api.ts
+++ api/api.ts
@@ -6,9 +6,7 @@
 
 import DeleterApiWorker from '@api/structures/DeleterApiWorker'
 import Discord from 'discord.js'
-import { FastifyRequest } from 'fastify'
 import FastifyCors from 'fastify-cors'
-import FastifyRateLimit from 'fastify-rate-limit'
 
 export default function api(manager: Discord.ShardingManager) {
 
@@ -16,24 +14,7 @@ export default function api(manager: Discord.ShardingManager) {
 
   return api.start(
     undefined,
-    [ FastifyCors, { origin: true } ],
-    [ FastifyRateLimit, {
-      global: false,
-      max: 100,
-      timeWindow: 3 * 60 * 1000,
-      cache: 5000,
-      keyGenerator: (req: FastifyRequest) => {
-        return req.headers?.['cf-connecting-ip'] ||
-          req.headers?.['x-forwarded-for'] ||
-          req.socket.remoteAddress
-      },
-      // addHeaders: {
-      //   'x-ratelimit-limit': true,
-      //   'x-ratelimit-remaining': true,
-      //   'x-ratelimit-reset': true,
-      //   'retry-after': false
-      // }
-    } ]
+    [ FastifyCors, { origin: true } ]
   )
 
 }
