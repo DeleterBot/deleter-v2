542a840cb57369c6f63178f8c53ff405f0c05c94
Mirdukkk <vdvmirduk@gmail.com>
2021-04-26 23:56:46 +0300

    breaking(api): change Guild structure

--- api/utils/makePartialGuild.ts
+++ api/utils/makePartialGuild.ts
@@ -1,20 +1,20 @@
 import { Constants } from 'discord.js'
 
-export default function makePartialGuild(guild: any, isNewGuild = false) {
+export default function makePartialGuild(guild: any, added = false) {
   if (guild.name === null || guild.name === undefined) return {
     id: guild.id,
     name: null,
-    iconURL: null,
-    newGuild: true
+    icon: null,
+    added: false
   }
 
   return {
     id: guild.id,
     name: guild.name,
-    iconURL: guild.icon
+    icon: guild.icon
       ? Constants.Endpoints.CDN(Constants.DefaultOptions.http!.cdn!).Icon(guild.id, guild.icon, 'png', 64)
       : null,
-    newGuild: isNewGuild
+    added: added
   }
 
 }
